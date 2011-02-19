3e1058eec92b2bc293cca61e50ddd9864c3f53b4
Jim Kubicek <jkubicek@gmail.com>
2011-02-19 12:53:57 -0500

    Modified the category to support SHA512.
    Also made it a bit more flexible, in case we want to add more hash types in the future.

--- NSString+Digest.h
+++ NSString+Digest.h
@@ -1,4 +1,13 @@
 @interface NSString (NSString_Digest)
+
+typedef enum {
+    NSStringDigestTypeMD5,
+    NSStringDigestTypeSHA1,
+    NSStringDigestTypeSHA512
+} NSStringDigestType;
+
 - (NSString *)MD5;
 - (NSString *)SHA1;
+
+- (NSString *)hashWithDigestType:(NSStringDigestType)type;
 @end
--- NSString+Digest.m
+++ NSString+Digest.m
@@ -6,25 +6,46 @@
 
 - (NSString *)MD5
 { 
-    const char *s = [self UTF8String];
-    unsigned char result[CC_MD5_DIGEST_LENGTH]; 
-    CC_MD5(s, strlen(s), result);
-    
-    NSMutableString *digest = [NSMutableString stringWithCapacity:(CC_MD5_DIGEST_LENGTH * 2)];
-    for (NSUInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
-        [digest appendFormat:@"%02x",result[i]];
-    
-    return [NSString stringWithString:digest];
+    return [self hashWithDigestType:NSStringDigestTypeMD5];
 }
 
 - (NSString *)SHA1
 { 
+    return [self hashWithDigestType:NSStringDigestTypeSHA1];
+}
+
+- (NSString *)hashWithDigestType:(NSStringDigestType)type {
     const char *s = [self UTF8String];
-    unsigned char result[CC_SHA1_DIGEST_LENGTH]; 
-    CC_SHA1(s, strlen(s), result);
+    int digestLength;
     
-    NSMutableString *digest = [NSMutableString stringWithCapacity:(CC_SHA1_DIGEST_LENGTH * 2)];
-    for (NSUInteger i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+    switch (type) {
+        case NSStringDigestTypeMD5:
+            digestLength = CC_MD5_DIGEST_LENGTH;
+            break;
+        case NSStringDigestTypeSHA1:
+            digestLength = CC_SHA1_DIGEST_LENGTH;
+            break;
+        case NSStringDigestTypeSHA512:
+            digestLength = CC_SHA512_DIGEST_LENGTH;
+            break;
+    }
+    
+    unsigned char result[digestLength]; 
+    
+    switch (type) {
+        case NSStringDigestTypeMD5:
+            CC_MD5(s, strlen(s), result);
+            break;
+        case NSStringDigestTypeSHA1:
+            CC_SHA1(s, strlen(s), result);
+            break;
+        case NSStringDigestTypeSHA512:
+            CC_SHA512(s, strlen(s), result);
+            break;
+    }
+    
+    NSMutableString *digest = [NSMutableString stringWithCapacity:(digestLength * 2)];
+    for (NSUInteger i = 0; i < digestLength; i++)
         [digest appendFormat:@"%02x",result[i]];
     
     return [NSString stringWithString:digest];
